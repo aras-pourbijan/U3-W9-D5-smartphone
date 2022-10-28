ef48b14f8f4a7060a3ebaaf018274b9f9718dd5e
Aras Pourbijan <arasmidos@gmail.com>
2022-10-28 14:22:16 +0200

    tsc funziona!

--- js/script.js
+++ js/script.js
@@ -1,17 +1,59 @@
 "use strict";
-class costum {
+document.addEventListener("DOMContentLoaded", () => {
+    let text = document.querySelector(".container p");
+});
+class navigatore {
+    constructor(marca, modello) {
+        this.marca = marca;
+        this.modello = modello;
+        this.credito = 0;
+        this.minutiConsumati = 0;
+        this.costoChiamata = 0.2;
+    }
+    ricarica(e) { }
+    chiamnata(m) { }
+    numero404() { }
+    getNumeroChiamate() { }
+    azzerraChiamate() { }
 }
-let myList = [];
-fetch("https://striveschool-api.herokuapp.com/books")
-    .then((res) => res.json())
-    .then((json) => getList(json))
-    .catch((err) => console.log(err));
-function getList(dati) {
-    dati.forEach((ele) => {
-        console.log(ele);
-        let {} = ;
-    });
+class cellulari extends navigatore {
 }
-// document.addEventListener("DOMContentLoaded", () => {
-//     let text = document.querySelector(".container p") as HTMLElement;
-// }
+class smartPhone extends cellulari {
+    constructor(marca, modello, scontoSullaChiamata) {
+        super(marca, modello);
+        this.scontoSullaChiamata = scontoSullaChiamata;
+    }
+    ricarica(e) {
+        this.credito += this.credito + e;
+        return console.log(`il tuo credito: ${this.credito}`);
+    }
+    chiamnata(m) {
+        if (this.credito >= 0.2) {
+            this.credito = this.credito - (m * this.costoChiamata * (100 - this.scontoSullaChiamata)) / 100;
+            this.minutiConsumati = this.minutiConsumati + m;
+        }
+        else {
+            console.log("non hai creddito sufficente , fai una ricarica per chiamare!");
+        }
+    }
+    numero404() {
+        this.credito < this.costoChiamata * 10
+            ? console.log(`il tuo credito : ${this.credito} ma attenzione hai pocco  credito, ti conviene effettuare una ricarica!`)
+            : console.log(`il tuo credito : ${this.credito}`);
+    }
+    getNumeroChiamate() {
+        return console.log(this.minutiConsumati);
+    }
+    azzerraChiamate() {
+        this.minutiConsumati = 0;
+        return console.log("hai azzerato i minuti consumati!");
+    }
+}
+let u1 = new smartPhone("samsung", "galaxy s22", 0);
+u1.ricarica(10);
+u1.chiamnata(1);
+u1.numero404();
+u1.chiamnata(3);
+u1.chiamnata(14);
+u1.chiamnata(53);
+u1.chiamnata(2);
--- ts/script.ts
+++ ts/script.ts
@@ -2,30 +2,78 @@ document.addEventListener("DOMContentLoaded", () => {
     let text = document.querySelector(".container p") as HTMLElement;
 });
 
-abstract class smartphone {
-    marca: string;
-    modello: string;
-    anno: number;
-    
+abstract class navigatore {
+    protected marca: string;
+    protected modello: string;
+    protected credito: number;
+    protected minutiConsumati: number;
+    protected costoChiamata: number;
 
-    constructor(marca: string,
-        modello: string,
-        anno: number){
-            this.marca=marca;
-            this.modello=modello
-            this.anno=anno
-        }
+    constructor(marca: string, modello: string) {
+        this.marca = marca;
+        this.modello = modello;
+        this.credito = 0;
+        this.minutiConsumati = 0;
+        this.costoChiamata = 0.2;
+    }
 
-        ricarica():void{
+    protected ricarica(e: number) {}
+    protected chiamnata(m: number) {}
 
-        }
-        chiamnata():void{
+    public numero404(): void {}
 
-        }
-        public numero404()=>(x:number) {
+    protected getNumeroChiamate(): void {}
 
-        }
-        ricarica():void{
-
-        }
+    protected azzerraChiamate(): void {}
 }
+
+abstract class cellulari extends navigatore {}
+
+class smartPhone extends cellulari {
+    scontoSullaChiamata: number;
+
+    constructor(marca: string, modello: string, scontoSullaChiamata: number) {
+        super(marca, modello);
+        this.scontoSullaChiamata = scontoSullaChiamata;
+    }
+
+    ricarica(e: number) {
+        this.credito += this.credito + e;
+        return console.log(`il tuo credito: ${this.credito}`);
+    }
+
+    chiamnata(m: number) {
+        if (this.credito >= 0.2) {
+            this.credito = this.credito - (m * this.costoChiamata * (100 - this.scontoSullaChiamata)) / 100;
+            this.minutiConsumati = this.minutiConsumati + m;
+        } else {
+            console.log("non hai creddito sufficente , fai una ricarica per chiamare!");
+        }
+    }
+
+    public numero404() {
+        this.credito < this.costoChiamata * 10
+            ? console.log(`il tuo credito : ${this.credito} ma attenzione hai pocco  credito, ti conviene effettuare una ricarica!`)
+            : console.log(`il tuo credito : ${this.credito}`);
+    }
+
+    getNumeroChiamate() {
+        return console.log(this.minutiConsumati);
+    }
+
+    azzerraChiamate() {
+        this.minutiConsumati = 0;
+        return console.log("hai azzerato i minuti consumati!");
+    }
+}
+
+let u1 = new smartPhone("samsung", "galaxy s22", 0);
+
+u1.ricarica(10);
+u1.chiamnata(1);
+u1.numero404();
+u1.chiamnata(3);
+
+u1.chiamnata(14);
+u1.chiamnata(53);
+u1.chiamnata(2);
